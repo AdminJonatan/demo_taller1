fe1d14e0aa4e409e95ab667cf1421a62e9e81aa3
AdminJonatan <Jonatan_gomez23191@elpoli.edu.co>
2022-02-10 13:08:50 -0500

    firstfd

--- src/main/java/co/com/pds/demo/repositorio/BookingsRepository.java
+++ src/main/java/co/com/pds/demo/repositorio/BookingsRepository.java
@@ -7,6 +7,6 @@ import co.com.pds.demo.entidades.*;
 public interface BookingsRepository   extends JpaRepository<Bookings, Long>{  //Extender la funciones  de la base de datos para spring como guardar,  eliminar, buscar, modificar
 
 
-    
+
 }
 
